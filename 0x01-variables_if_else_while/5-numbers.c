efe809809e64efd24f52511e6cee5063357489c8
beshoyqulta <beshoyashraf20162016@gmail.com>
2023-03-17 13:25:32 -0700

    Commit

---
+++ 0x01-variables_if_else_while/5-numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 1-9
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	for (int i = 0; i <= 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
