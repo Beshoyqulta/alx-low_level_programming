b26f720e5d057e7537e0e514d8245ad0be1c9aa0
beshoyqulta <beshoyashraf20162016@gmail.com>
2023-03-17 09:33:41 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,8 @@
  *
  * comment
  */
-int main(void){
-
-
-puts("\"Programming is like building a multilingual puzzle");
-return 0;
-
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
