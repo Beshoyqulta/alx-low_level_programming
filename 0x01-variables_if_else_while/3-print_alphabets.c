f02d38624b23dddf9bb8d85663321e77e8706f6f
beshoyqulta <beshoyashraf20162016@gmail.com>
2023-03-17 12:59:14 -0700

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+ * main - print all the alphabets
+ * follwed by a line
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	char c;
+	char c1;
+	for(c = 'a'; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	for(c1 = 'A'; c1 <= '$' ; c1++)
+	{
+		putchar(c1);
+	}
+	putchar('\n');
+	return (0);
+
+}
