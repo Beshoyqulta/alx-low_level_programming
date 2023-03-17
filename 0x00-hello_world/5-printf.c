4dfc9c774d2b08461ced5f8ae1cf1e9bd800ecd8
beshoyqulta <beshoyashraf20162016@gmail.com>
2023-03-17 10:27:37 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-/*
- * printing a phrase with printf
- * return 0
- */
+
+/**
+ *  * main - Entry point
+ *  * print using printf
+ *  * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+
 }
