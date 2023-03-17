7b3f262e86cf3c11527ea1ee8e48d3a710e84a62
beshoyqulta <beshoyashraf20162016@gmail.com>
2023-03-17 13:14:02 -0700

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio>
+/**
+ * main - print all alphabet except q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
