93c80eae1696c61b4733a684ab9cf059e4b68508
beshoyqulta <beshoyashraf20162016@gmail.com>
2023-03-17 10:16:58 -0700

    Commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /*
- * 
- * 
  * printing a phrase with printf
  * return 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
