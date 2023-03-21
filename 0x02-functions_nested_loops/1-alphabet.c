fe42dfb90b31fd5e128e95bf9636bf12eb194d97
beshoyqulta <beshoyashraf20162016@gmail.com>
2023-03-21 14:44:06 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * main - print alphabet
- * return : Always 0 (succesful)
+ * main - print alphabet , followed by new line
+ * Return: Always 0 (succesful)
  */
 int main(void)
 {
-	char i ;
+	char i;
 
-	for(i = 'a' ;i < 'z' ; i++
+	for (i = 'a'; i < 'z'; i++)
 	{
-		_putchar('i');			
+		_putchar('i');
 	}
 	_putchar('\n');
 	return (0);
