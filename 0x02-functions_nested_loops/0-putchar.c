37fa1b45f741bbabb11861e31ca9136d3d7aaa17
beshoyqulta <beshoyashraf20162016@gmail.com>
2023-03-21 14:31:16 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main - print putchar
  * Return: Always 0 (successful)
 */
-int main(void) 
+int main(void)
 {
-	printf("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 
 }
