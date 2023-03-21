ca0d8e609bd99aa9064edc11985f974e7e5fc632
beshoyqulta <beshoyashraf20162016@gmail.com>
2023-03-21 14:47:05 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,15 @@
  * Return: Always 0 (succesful)
  */
 int main(void)
-{
+{	void print_alphabet(void)
+	{
 	char i;
 
-	for (i = 'a'; i < 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar('i');
 	}
 	_putchar('\n');
 	return (0);
+	}
 }
