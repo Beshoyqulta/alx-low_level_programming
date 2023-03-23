25140ef4305f234d74c500dda9372f58275f991a
beshoyqulta <beshoyashraf20162016@gmail.com>
2023-03-23 09:21:27 -0700

    message

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper to check the upper 
+ * @c : to be checked
+ * Return : 1 if upper 0 if lower
+ */
+int _isupper(int c)
+{
+	return ( c >= 'A' && c <= 'Z');
+}
