2c72b711b6c0f68cfea5cd17a13051c75a3f7e11
beshoyqulta <beshoyashraf20162016@gmail.com>
2023-03-23 09:26:05 -0700

    commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper to check the upper 
+ * _isupper -  to check the upper
  * @c : to be checked
- * Return : 1 if upper 0 if lower
+ * Return: 1 if upper 0 if lower
  */
 int _isupper(int c)
 {
-	return ( c >= 'A' && c <= 'Z');
+	return (c >= 'A' && c <= 'Z');
 }
