3a7464ed6e0e72f0a2f29ec9853aea1be7b7093e
beshoyqulta <beshoyashraf20162016@gmail.com>
2023-03-23 14:34:20 -0700

    Commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints number from 0 to 9
+ * except 2 and 4 , then add a new line
+ */
+void print_most_numbers(void)
+{
+	int i;
+	for (i=0; i < 10; i++)
+	{
+		if(i == 2 || i == 4)
+		{
+			continue;
+		}
+		_putchat(i +'0');
+	}
+	_putchar('\n');
+}
